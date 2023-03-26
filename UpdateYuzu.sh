5d01a884fa034a8ba189b7022246783cea528c6c
Alec Custer <alcuster@icloud.com>
2023-03-26 11:47:29 -0700

    use var for path, add permissions to downloaded file

--- UpdateYuzu.sh
+++ UpdateYuzu.sh
@@ -1,3 +1,8 @@
 #! /bin/bash
-curl -s https://api.github.com/repos/pineappleEA/pineapple-src/releases/latest | jq -r ".assets[0] | .browser_download_url" | wget -qO /path/to/yuzu.AppImage -i -
-/path/to/yuzu.AppImage
+YUZU_PATH=/home/deck/Applications/yuzu.AppImage
+# Download latest Yuzu EA
+curl -s https://api.github.com/repos/pineappleEA/pineapple-src/releases/latest | jq -r ".assets[0] | .browser_download_url" | wget -qO $YUZU_PATH -i -
+# Give it executable permissions
+chmod +x $YUZU_PATH
+# Launch Yuzu
+$YUZU_PATH
