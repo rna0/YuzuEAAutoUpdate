9ca791afc278ced8b58390cd4b71564f051c89f8
theSt33v <steve.wenzl@gmail.com>
2023-05-10 18:57:30 -0400

    Added changes submitted by ddrmatt32

--- UpdateYuzu.sh
+++ UpdateYuzu.sh
@@ -1,8 +1,16 @@
 #! /bin/bash
-YUZU_PATH=/home/deck/Applications/yuzu.AppImage
+
+YUZU_PATH=/home/deck/Documents/yuzu.AppImage
+
+if ping -q -c 1 -W 1 google.com >/dev/null; then
+
 # Download latest Yuzu EA
 curl -s https://api.github.com/repos/pineappleEA/pineapple-src/releases/latest | jq -r ".assets[0] | .browser_download_url" | wget -qO $YUZU_PATH -i -
+
 # Give it executable permissions
 chmod +x $YUZU_PATH
+
+fi
+
 # Launch Yuzu
 $YUZU_PATH
